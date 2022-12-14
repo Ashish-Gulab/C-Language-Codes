cd8f84768e56de694ad4efadc02aa76ec5e9febd
Ashish Gulab <ashishgulab.gulab@gmail.com>
2022-12-14 18:25:24 +0530

    Create Pattern Printing1.c

---
+++ Pattern Printing1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    
+     int x,j;
+    
+    for(x=1;x<=4;x++)
+    {
+        printf("\n ");
+        for(j=x;j>0;j--)
+        {
+            printf("%d ",j);
+        }
+        
+    }
+    return 0;
+}
