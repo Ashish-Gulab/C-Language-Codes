ff3ef2eda0fc7a1499b7742e0684d82615491e3a
Ashish Gulab <ashishgulab.gulab@gmail.com>
2022-12-16 19:14:24 +0530

    Create Print2dArray.c

---
+++ Print2dArray.c
@@ -0,0 +1,20 @@
+#include<stdio.h>  
+
+int main()
+{      
+  int i=0,j=0;    
+  int arr[4][3]={{1,2,3},{2,3,4},{3,4,5},{4,5,6}};     
+
+  for(i=0;i<4;i++)
+    {    
+      for(j=0;j<3;j++)
+      {    
+        printf(" %d ",arr[i][j]);    
+      }
+      printf("\n");
+    }
+  return 0;  
+}    
+
+
+
