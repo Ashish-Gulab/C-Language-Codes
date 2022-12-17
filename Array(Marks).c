3ffa1f28180d67fa772daf05bc694c07289d1970
Ashish Gulab <ashishgulab.gulab@gmail.com>
2022-12-17 22:02:47 +0530

    Create Array(Marks).c

---
+++ Array(Marks).c
@@ -0,0 +1,17 @@
+#include<stdio.h>  
+
+int main()    
+{ 
+    int i;
+    int marks[5];
+    marks[0]= 76;
+    marks[1]= 76;
+    marks[2]= 76;
+    marks[3]= 76;
+    marks[4]= 76;
+    for(i=0;i<5;i++)
+    { 
+        printf("%d",marks[i]);
+    }
+    return 0;
+}
