6130ce106e64eab0ccc2d3bbc4d2de051e74175d
Ashish Gulab <ashishgulab.gulab@gmail.com>
2022-12-17 22:43:32 +0530

    Create StringDeclaration.c

---
+++ StringDeclaration.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main ()
+{ 
+    char ch1[7]= {'a','a','r','u','s','h','i','\0'};
+    char ch2[7] = "Gulab";
+    printf("Char Array Value is %s\n",ch1);
+    printf("String Literal value is %s\n",ch2);
+    return 0;
+}
