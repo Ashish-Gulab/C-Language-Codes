32b5e3d7baa5a51d460bb291e583ada5d3df1720
Ashish Gulab <ashishgulab.gulab@gmail.com>
2022-12-17 22:09:22 +0530

    Create MathFunctions.c

---
+++ MathFunctions.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main ()
+{
+printf("\n%f",ceil(2.6));
+printf("\n%f",ceil(2.3));
+printf("\n%f",floor(4.6));
+printf("\n%f", floor (4.2));
+printf("\n%f",sqrt(25));
+printf("\n%f",sqrt(36));
+printf("\n%f",sqrt(11));
+printf("\n%f",pow(3,4));
+printf("\n%f",pow(2,3));
+printf("\n%f",pow(3,3));
+printf("\n%d",abs(-11));
+printf("\n%d",abs(-20));
+return 0;
+
+}
