cd14b8ab1454c720f9c347439ea50f3608e9a6d6
Ashish Gulab <ashishgulab.gulab@gmail.com>
2022-12-18 10:36:03 +0530

    Create FibonacciSeries.c

---
+++ FibonacciSeries.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+   int a=0, b=1, c, i, n;
+   printf("Enter the number of elements");
+   scanf("%d",&n);
+   printf("\n%d%d",a,b);
+
+   for(i = 1; i <= n-2; i++) {
+      c = a + b;
+      printf("%d ", c);
+
+      a = b;
+      b = c;
+   }
+
+   return 0;
+}
