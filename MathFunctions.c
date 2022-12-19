92b6f95531f2a979068f06156d08a041cc532090
Ashish Gulab <ashishgulab.gulab@gmail.com>
2022-12-19 21:56:57 +0530

    Update MathFunctions.c

--- MathFunctions.c
+++ MathFunctions.c
@@ -2,18 +2,45 @@
 #include<string.h>
 int main ()
 {
-printf("\n%f",ceil(2.6));
-printf("\n%f",ceil(2.3));
-printf("\n%f",floor(4.6));
-printf("\n%f", floor (4.2));
-printf("\n%f",sqrt(25));
-printf("\n%f",sqrt(36));
-printf("\n%f",sqrt(11));
-printf("\n%f",pow(3,4));
-printf("\n%f",pow(2,3));
-printf("\n%f",pow(3,3));
-printf("\n%d",abs(-11));
-printf("\n%d",abs(-20));
-return 0;
-
+  printf("\n%f",ceil(2.6));
+  printf("\n%f",ceil(2.3));
+  printf("\n%f",floor(4.6));
+  printf("\n%f", floor (4.2));
+  printf("\n%f",sqrt(25));
+  printf("\n%f",sqrt(36));
+  printf("\n%f",sqrt(11));
+  printf("\n%f",pow(3,4));
+  printf("\n%f",pow(2,3));
+  printf("\n%f",pow(3,3));
+  printf("\n%d",abs(-11));
+  printf("\n%d",abs(-20));
+  printf("\n%f",ceil(8.7));
+  printf("\n%f",ceil(8.3));
+  printf("\n%f",ceil(6.7));
+  printf("\n%f",ceil(6.1));
+  printf("\n%f",ceil(10.1));
+  printf("\n%f",ceil(78.5));
+  printf("\n%f",floor(66.5));
+  printf("\n%f",floor(66.8));
+  printf("\n%f",floor(88.4));
+  printf("\n%f",floor(3.6));
+  printf("\n%f", floor (8.7));
+  printf("\n%f",sqrt(16));
+  printf("\n%f",sqrt(28));
+  printf("\n%f",sqrt(180));
+  printf("\n%f",sqrt(87));
+  printf("\n%f",sqrt(95));
+  printf("\n%f",sqrt(34));
+  printf("\n%f",pow(3,7));
+  printf("\n%f",pow(6,7));
+  printf("\n%f",pow(4,4));
+  printf("\n%f",pow(3,4));
+  printf("\n%f",pow(5,3));
+  printf("\n%f",pow(7,2));
+  printf("\n%d",abs(-11));
+  printf("\n%d",abs(-76));
+  printf("\n%d",abs(-67));
+  printf("\n%d",abs(-98));
+  printf("\n%d",abs(-54));
+  return 0;
 }
