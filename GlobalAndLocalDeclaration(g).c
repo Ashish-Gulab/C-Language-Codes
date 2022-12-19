8181ee77de51f1025a26a6a5336f9050c14ac3c9
Ashish Gulab <ashishgulab.gulab@gmail.com>
2022-12-19 21:42:58 +0530

    Create GlobalAndLocalDeclaration(g).c

---
+++ GlobalAndLocalDeclaration(g).c
@@ -0,0 +1,22 @@
+/*#include <stdio.h> 
+int g = 30;
+int main()
+
+{ int ;
+
+printf("value of g=%d",g);
+
+return 0;
+}
+
+*/#include<stdio.h>
+
+int main ()
+
+{
+ int a= 2 ,b= 3,g ;
+
+ g=a+b;
+
+printf("value of g is %d",g);
+return 0;}
