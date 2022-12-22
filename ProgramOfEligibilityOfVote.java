9bac65cb2d11dee145c5046eb06aad2dbb8e5250
Ashish Gulab <ashishgulab.gulab@gmail.com>
2022-12-22 18:30:44 +0530

    Create ProgramOfEligibilityOfVote.java

---
+++ ProgramOfEligibilityOfVote.java
@@ -0,0 +1,17 @@
+#include <stdio.h>  
+int main()  
+{  
+   int a;
+  printf("Enter the age ");
+
+  scanf("%d",&a);
+
+   if(a>=18)
+
+ printf(" person is eligible for vote");
+ 
+ else
+printf("person is not eligible for vote");
+ 
+ return 0;
+}
