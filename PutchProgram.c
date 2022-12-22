9dd6a791906749096507484914239b20975ea9e8
Ashish Gulab <ashishgulab.gulab@gmail.com>
2022-12-22 18:47:50 +0530

    Create PutchProgram.c

---
+++ PutchProgram.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    char ch;
+    clrscr();
+    printf("Enter any character");
+    ch=getch();
+    printf("\nEntered character is  ");
+    putch(ch);
+    getch();
+}
