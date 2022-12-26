8d30aa0039c355b7ee03d87d1995ed50b5c52f5a
Ashish Gulab <ashishgulab.gulab@gmail.com>
2022-12-26 22:05:33 +0530

    Create NumbersOfVowelInString.c

---
+++ NumbersOfVowelInString.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main ()
+{
+  char chars[7] = "Gulab";
+  int i=0;
+  int count=0;
+  while(i<7)
+  if(s[i]=='a'|| s[i]=='e'|| s[i]=='i'||s[i]=='o'||s[i]=='u' ) 
+  { 
+    count++;
+  }
+  i++;
+
+  printf("The number of vovels is %d",count);
+}
