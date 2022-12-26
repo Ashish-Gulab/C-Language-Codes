92297f0331af883c33b3d109fee26c566a835399
Ashish Gulab <ashishgulab.gulab@gmail.com>
2022-12-26 22:03:04 +0530

    Create strcompStringProgram.c

---
+++ strcompStringProgram.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main ()
+{
+  char str1[10],str2[10];
+  printf("Enter first string");
+  gets(str1);
+  printf("Enter the second string");
+  gets(str2);
+  if(strcmp(str1,str2)==0)
+  {
+    printf("Strings are equal");}
+  else
+  { 
+    printf("Strings are not equal");
+  }
+  return 0;
+}
