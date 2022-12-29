8442637de1fd8aebe78c9b48029a44de16d905d6
Ashish Gulab <ashishgulab.gulab@gmail.com>
2022-12-29 22:48:06 +0530

    Create UseOfAnotherFunction.c

---
+++ UseOfAnotherFunction.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,c;
+  printf("Enter the number of a and b");
+  scanf("%d%d",&a,&b);
+  c=((a!=b)&&(a<=b));
+  printf("print value of %d",c);
+}
+
+/*
+#include<stdio.h>
+int argentina()
+{
+  int d,e,f;
+  printf("Enter the number of a and b");
+  scanf("%d%d",&d,&e);
+  f=((d!=e)||(d<=e));
+  printf("print value of %d",f);
+}
+*/
